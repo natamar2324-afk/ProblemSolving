a2529107f9a7d12e5638886d018ac7c3e2039fed
natamar2324-afk <natamar2324@gmail.com>
2025-10-21 19:55:02 +0100

    Update week1.py

--- week1.py
+++ week1.py
@@ -1,23 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2865
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;}
-{\colortbl;\red255\green255\blue255;\red71\green80\blue91;\red244\green246\blue249;\red24\green26\blue30;
-\red10\green58\blue158;\red82\green32\blue172;\red11\green34\blue86;\red194\green11\blue35;}
-{\*\expandedcolortbl;;\cssrgb\c34902\c38824\c43137;\cssrgb\c96471\c97255\c98039;\cssrgb\c12157\c13725\c15686;
-\cssrgb\c1961\c31373\c68235;\cssrgb\c40000\c22353\c72941;\cssrgb\c3922\c18824\c41176;\cssrgb\c81176\c13333\c18039;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs27\fsmilli13600 \cf2 \cb3 \expnd0\expndtw0\kerning0
-# Exercise 1: largest of two numbers (with equal case)\cf4 \
-a \cf5 =\cf4  \cf6 float\cf4 (\cf6 input\cf4 (\cf7 "Enter the first number (A): "\cf4 ))   \cf2 # read A as a number (accepts decimals)\cf4 \
-b \cf5 =\cf4  \cf6 float\cf4 (\cf6 input\cf4 (\cf7 "Enter the second number (B): "\cf4 ))  \cf2 # read B as a number\cf4 \
-\
-\cf2 # Compare the two numbers and print which is largest or if they are equal\cf4 \
-\pard\pardeftab720\partightenfactor0
-\cf8 if\cf4  a \cf5 >\cf4  b:\
-    \cf6 print\cf4 (\cf7 "The largest number is"\cf4 , a)\
-\cf8 elif\cf4  b \cf5 >\cf4  a:\
-    \cf6 print\cf4 (\cf7 "The largest number is"\cf4 , b)\
-\cf8 else\cf4 :\
-    \cf6 print\cf4 (\cf7 "Both numbers are equal:"\cf4 , a)}
+# This is called block comment
+print("Hello World!")
+# print("\n")
+print("Welcome to QHO444") # This is an inline comment
