de8b1c6684f16a0c4cdc69c894741466403b5b47
natamar2324-afk <natamar2324@gmail.com>
2026-02-04 21:13:20 +0000

    Update process.py
    Receive the overall dataset and 
    perform necessary processes

--- assessment/process.py
+++ assessment/process.py
@@ -1,6 +1,6 @@
 """
-This module is responsible for processing the data.  It will largely contain functions that will recieve the overall dataset and 
-perfrom necessary processes in order to provide the desired result in the desired format.
+This module is responsible for processing the data.  It will largely contain functions that will receive the overall dataset and 
+perform necessary processes in order to provide the desired result in the desired format.
 It is likely that most sections will require functions to be placed in this module.
 """
 import csv
