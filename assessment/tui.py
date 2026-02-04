e5eeb8407940cdad318d55e68d9f0d18bb1f66cd
natamar2324-afk <natamar2324@gmail.com>
2026-02-04 21:14:04 +0000

    Update tui.py
    Utilise any parameters and perform user input/output

--- assessment/tui.py
+++ assessment/tui.py
@@ -4,7 +4,7 @@ The functions in this module will display information to the user and/or retriev
 Each function in this module should utilise any parameters and perform user input/output.
 A function may also need to format and/or structure a response e.g. return a list, tuple, etc.
 Any errors or invalid inputs should be handled appropriately.
-Please note that you do not need to read the data file or perform any other such processing in this module.
+Please note that you do not need to read the data file or perform any other such processing in this module. 
 """
 
 def display_welcome():
