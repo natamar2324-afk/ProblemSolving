6cfaaa3fceab599d00033cc2cda84a672f2ae947
natamar2324-afk <natamar2324@gmail.com>
2026-02-04 21:15:14 +0000

    Update visual.py
    Visualising the data using Matplotlib

--- assessment/visual.py
+++ assessment/visual.py
@@ -1,5 +1,5 @@
 """
-This module is responsible for visualising the data using Matplotlib.
+This module is responsible for visualising the data using Matplotlib. 
 Any visualisations should be generated via functions in this module.
 """
 import matplotlib.pyplot as plt
